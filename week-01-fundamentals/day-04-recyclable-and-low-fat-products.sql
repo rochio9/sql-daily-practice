393afbe2198e54f006d17f2004b844eaf1cde128
Rocío Yáñez <rocioyaez99@gmail.com>
2026-05-07 16:28:06 -0400

    feat(weak-o1):solve Recyclable and Low Fat Products

---
+++ week-01-fundamentals/day-04-recyclable-and-low-fat-products.sql
@@ -0,0 +1,41 @@
+-- =============================================================
+-- Ejercicio: Big Countries
+-- Plataforma: LeetCode (Easy)
+-- URL: https://leetcode.com/problems/recyclable-and-low-fat-products/
+-- Fecha: 2026-05-07
+-- =============================================================
+
+-- Enunciado:
+-- Name table: `Products`
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | product_id  | int     |
+-- | low_fats    | enum    |
+-- | recyclable  | enum    |
+-- +-------------+---------+
+-- product_id is the primary key (column with unique values) for this 
+--table.
+-- low_fats is an ENUM (category) of type ('Y', 'N') where 'Y' means 
+--this product is low fat and 'N' means it is not.
+-- recyclable is an ENUM (category) of types ('Y', 'N') where 'Y' means 
+--this product is recyclable and 'N' means it is not.
+
+-- Write a solution to find the ids of products that are both low fat
+-- and recyclable.
+
+-- Return the result table in any order.
+
+-- =============================================================
+-- Solución (MySQL)
+-- =============================================================
+SELECT product_id 
+FROM Products 
+WHERE low_fats = 'Y' 
+   AND recyclable = 'Y';
+
+-- Se aplicó el filtro lógico AND (en comparación del día anterior donde se 
+-- aplicó el filtro lógico OR) para seleccionar solo los productos que 
+-- cumplen ambas condiciones (ser bajos en grasas y reciclables).
